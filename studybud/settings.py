0d252ebe072a03e199cc9bb39ee137cec24a0f95
biszaal <biszaal@yahoo.com>
2022-06-26 18:31:37 +0545

    heroku setup

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_w78s05y60&z0hdk#fz(w=0iis12va!-evv*vn+s#esa+mfvji'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studybud-tutorial.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
